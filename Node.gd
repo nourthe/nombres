1102aacd5afb1d5c3265f6fd4997a3d9d14ed395
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2018-11-19 12:43:21 -0300

    Creados menú e intro
    también edite otra vez gitignore

---
+++ Node.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func on_salir():
+	get_tree().quit()
+	pass # replace with function body
+
+
+func on_super():
+	get_tree().change_scene("res://assets/supervivencia.tscn")
+	pass # replace with function body
