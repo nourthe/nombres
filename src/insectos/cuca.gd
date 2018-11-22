72ddbb61e6761f05625b445e2865d7db44204b0f
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2018-11-22 12:24:07 -0300

    Comenzando abstracción

---
+++ src/insectos/cuca.gd
@@ -0,0 +1,7 @@
+extends "res://src/insectos/insecto.gd"
+
+#class_name Cuca, "res://assets/insecto.png"
+
+
+func _ready():
+	pass
--- src/insectos/insecto.gd
+++ src/insectos/insecto.gd
@@ -1,21 +1,14 @@
 extends KinematicBody2D
 
-class_name Insecto, "res://assets/insecto.png"
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 var LIMITE_ROTACION = 0.1
 var VELOCIDAD = 250
 var rotacion = 0
 var variacion = 0
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
 func _process(delta):
+	rotarYMover()
+	
+func rotarYMover():
 	randomize()
 	variacion = rand_range(-1,1)*delta
 	if(abs(rotacion + variacion) <= LIMITE_ROTACION):
@@ -23,5 +16,5 @@ func _process(delta):
 	rotate(rotacion)
 	var collision = move_and_collide(Vector2(0,-1).rotated(rotation)*VELOCIDAD*delta)
 	if collision:
-		if collision.collider.has_method("hit"):
-			collision.collider.hit(collision.normal)
+		#innecesario: if collision.collider.has_method("hit"):
+		collision.collider.hit(collision.normal)
