7869146eefc0b5466a7ec402e4d33d9319153d6f
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2019-01-25 12:57:24 -0300

    Añadida cucaracha animada en intro y super

--- src/insecto.gd
+++ src/insecto.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
 
+export var corriendo = true
 var LIMITE_ROTACION = 0.1
 var VELOCIDAD = 250
 var rotacion = 0
 var variacion = 0
 
 func _process(delta):
-	randomize()
-	variacion = rand_range(-1,1)*delta
-	if(abs(rotacion + variacion) <= LIMITE_ROTACION):
-		rotacion += variacion
-	rotate(rotacion)
-	var collision = move_and_collide(Vector2(0,-1).rotated(rotation)*VELOCIDAD*delta)
-	if collision:
-		if collision.collider.has_method("hit"):
-			collision.collider.hit(collision.normal)
+	if(corriendo):
+		randomize()
+		variacion = rand_range(-1,1)*delta
+		if(abs(rotacion + variacion) <= LIMITE_ROTACION):
+			rotacion += variacion
+		rotate(rotacion)
+		var collision = move_and_collide(Vector2(0,-1).rotated(rotation)*VELOCIDAD*delta)
+		if collision:
+			if collision.collider.has_method("hit"):
+				collision.collider.hit(collision.normal)
