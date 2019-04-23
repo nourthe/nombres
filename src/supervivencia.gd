d07f007487c5a9df5b17b3b8d026cef01cfaa1b6
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2019-04-23 00:43:59 -0300

    Limpieza de Codigo (src/supervivencia.gd)

--- src/supervivencia.gd
+++ src/supervivencia.gd
@@ -1,7 +1,8 @@
 extends "motor.gd"
 
 # Duracion de cada etapa en segundos
-var DURACION_ETAPA = 6
+export var DURACION_ETAPA = 6
+export(float, 0, 1) var PROBABILIDAD_INSECTICIDA = 0.3
 
 var etapa
 
@@ -12,13 +13,7 @@ func _ready():
 func _process(delta):
 	actualizarTextos()
 	jugador.puntos += delta
-	if jugador.puntos >= etapa*DURACION_ETAPA :
-		addRandObject("insecto")
-		etapa += 1
-		randomize()
-		var rand = rand_range(0,3)
-		if(rand<1):
-			addRandObject("insecticida")
+	
 	if jugador.vidas < 1 :
 		gameOver()
 
@@ -26,3 +21,12 @@ func actualizarTextos():
 	$textos/puntos.set_text("Puntaje: " + String(jugador.get_puntos()))
 	$textos/vidas.set_text("Vidas: " + String(jugador.vidas))
 	$textos/disparos.set_text("Disparos: " + String(jugador.disparos))
+
+func actualizarEtapa():
+	if jugador.puntos >= etapa*DURACION_ETAPA :
+		addRandObject("insecto")
+		etapa += 1
+		randomize()
+		var rand = rand_range(0,100)
+		if rand < PROBABILIDAD_INSECTICIDA*100 :
+			addRandObject("insecticida")
